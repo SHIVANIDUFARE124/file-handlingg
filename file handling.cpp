2f755430ea0c624e2f9daebee8c506d2cf4fc85e
SHIVANIDUFARE124 <66694740+SHIVANIDUFARE124@users.noreply.github.com>
2021-08-10 10:58:53 +0530

    Add files via upload

---
+++ file handling.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+	ofstream newFile("C:\\Users\\Hp\\Desktop\\CPP.txt");
+	cout<<"File Created...";
+	newFile<<"File handling created....";
+	newFile.close();
+	return 0;
+	
+}
